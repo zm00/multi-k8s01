6e91d0a0a13df439096f7866569b81d490a690bc
Sze Mun Foo (AP) <sze-mun.foo@global.ntt>
2020-09-08 13:47:40 +0800

    New Chapter

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './kids.jpg';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -12,13 +12,13 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Hello Princessess!</h1>
+            <h1 className="App-title">Hello K8S! versioning</h1>
             <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+            <Link to="/OtherPage">Other Page</Link>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Route path="/OtherPage" component={OtherPage} />
           </div>
         </div>
       </Router>
