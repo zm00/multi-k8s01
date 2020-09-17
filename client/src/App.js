3ed7e63901016631672e99fcfaed7ee79899ece9
Sze Mun Foo (AP) <sze-mun.foo@global.ntt>
2020-09-17 22:18:20 +0800

    Changed logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './Green.jpg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Hello K8S! Final!</h1>
+            <h1 className="App-title">Hello K8S!</h1>
             <Link to="/">Home</Link>
             <Link to="/OtherPage">Other Page</Link>
           </header>
